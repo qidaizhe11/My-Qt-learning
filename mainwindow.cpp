d9af6cba7c6b66e1f466e1e12e072517596589c1
Qi Diazhe <qidaizhe11@gmail.com>
2013-10-24 10:22:08 +0800

    Version 1.4.2
    QComboBox initialize complete.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -12,9 +12,84 @@ MainWindow::MainWindow(QWidget *parent)
     shapeComboBox->addItem(tr("Polygon"), RenderArea::Polygon);
     shapeComboBox->addItem(tr("Rectangle"), RenderArea::Rect);
     shapeComboBox->addItem(tr("Rounded Rectangle"), RenderArea::RoundedRect);
+    shapeComboBox->addItem(tr("Ellipse"), RenderArea::Ellipse);
+    shapeComboBox->addItem(tr("Pie"), RenderArea::Pie);
+    shapeComboBox->addItem(tr("Chord"), RenderArea::Chord);
+    shapeComboBox->addItem(tr("Path"), RenderArea::Path);
+    shapeComboBox->addItem(tr("Line"), RenderArea::Line);
+    shapeComboBox->addItem(tr("Polyline"), RenderArea::Polyline);
+    shapeComboBox->addItem(tr("Arc"), RenderArea::Arc);
+    shapeComboBox->addItem(tr("Points"), RenderArea::Points);
+    shapeComboBox->addItem(tr("Text"), RenderArea::Text);
+    shapeComboBox->addItem(tr("Pixmap"), RenderArea::Pixmap);
 
     shapeLabel = new QLabel(tr("Shape:"));
     shapeLabel->setBuddy(shapeComboBox);
+
+    penWidthSpinBox = new QSpinBox;
+    penWidthSpinBox->setRange(0, 20);
+    penWidthSpinBox->setSpecialValueText(tr("0 (cosmetic pen"));
+
+    penWidthLabel = new QLabel(tr("Pen &Width:"));
+    penWidthLabel->setBuddy(penWidthSpinBox);
+
+    penStyleComboBox = new QComboBox;
+    penStyleComboBox->addItem(tr("Solid"), static_cast<int>(Qt::SolidLine));
+    penStyleComboBox->addItem(tr("Dash"), static_cast<int>(Qt::DashLine));
+    penStyleComboBox->addItem(tr("Dot"), static_cast<int>(Qt::DotLine));
+    penStyleComboBox->addItem(tr("Dash Dot"), static_cast<int>(Qt::DashDotLine));
+    penStyleComboBox->addItem(tr("Dash Dot Dot"), static_cast<int>(Qt::DashDotDotLine));
+    penStyleComboBox->addItem(tr("None"), static_cast<int>(Qt::NoPen));
+
+    penStyleLabel = new QLabel(tr("&Pen Style:"));
+    penStyleLabel->setBuddy(penStyleComboBox);
+
+    penCapComboBox = new QComboBox;
+    penCapComboBox->addItem(tr("Flat"), Qt::FlatCap);
+    penCapComboBox->addItem(tr("Square"), Qt::SquareCap);
+    penCapComboBox->addItem(tr("Round"), Qt::RoundCap);
+
+    penCapLabel = new QLabel(tr("Pen &Cap:"));
+    penCapLabel->setBuddy(penCapComboBox);
+
+    penJoinComboBox = new QComboBox;
+    penJoinComboBox->addItem(tr("Miter"), Qt::MiterJoin);
+    penJoinComboBox->addItem(tr("Bevel"), Qt::BevelJoin);
+    penJoinComboBox->addItem(tr("Round"), Qt::RoundJoin);
+
+    penJoinLabel = new QLabel(tr("Pen &Join:"));
+    penJoinLabel->setBuddy(penJoinComboBox);
+
+    brushStyleComboBox = new QComboBox;
+    brushStyleComboBox->addItem(tr("Linear Gradient"),
+                                static_cast<int>(Qt::LinearGradientPattern));
+    brushStyleComboBox->addItem(tr("Radial Gradient"),
+                                static_cast<int>(Qt::RadialGradientPattern));
+    brushStyleComboBox->addItem(tr("Conical Gradient"),
+                                static_cast<int>(Qt::ConicalGradientPattern));
+    brushStyleComboBox->addItem(tr("Texture"), static_cast<int>(Qt::TexturePattern));
+    brushStyleComboBox->addItem(tr("Solid"), static_cast<int>(Qt::SolidPattern));
+    brushStyleComboBox->addItem(tr("Horizontal"), static_cast<int>(Qt::HorPattern));
+    brushStyleComboBox->addItem(tr("Vertical"), static_cast<int>(Qt::VerPattern));
+    brushStyleComboBox->addItem(tr("Cross"), static_cast<int>(Qt::CrossPattern));
+    brushStyleComboBox->addItem(tr("Backward Diagonal"), static_cast<int>(Qt::BDiagPattern));
+    brushStyleComboBox->addItem(tr("Forward Diagonal"), static_cast<int>(Qt::FDiagPattern));
+    brushStyleComboBox->addItem(tr("Diagonal Cross"), static_cast<int>(Qt::DiagCrossPattern));
+    brushStyleComboBox->addItem(tr("Dense 1"), static_cast<int>(Qt::Dense1Pattern));
+    brushStyleComboBox->addItem(tr("Dense 2"), static_cast<int>(Qt::Dense2Pattern));
+    brushStyleComboBox->addItem(tr("Dense 3"), static_cast<int>(Qt::Dense3Pattern));
+    brushStyleComboBox->addItem(tr("Dense 4"), static_cast<int>(Qt::Dense4Pattern));
+    brushStyleComboBox->addItem(tr("Dense 5"), static_cast<int>(Qt::Dense5Pattern));
+    brushStyleComboBox->addItem(tr("Dense 6"), static_cast<int>(Qt::Dense6Pattern));
+    brushStyleComboBox->addItem(tr("Dense 7"), static_cast<int>(Qt::Dense7Pattern));
+    brushStyleComboBox->addItem(tr("None"), static_cast<int>(Qt::NoBrush));
+
+    brushStyleLabel = new QLabel(tr("&Brush:"));
+    brushStyleLabel->setBuddy(brushStyleComboBox);
+
+    otherOptionsLabel = new QLabel(tr("Options:"));
+    antialiasingCheckBox = new QCheckBox(tr("&Antialiasing"));
+    transformationsCheckBox = new QCheckBox(tr("&Transformations"));
 }
 
 MainWindow::~MainWindow()
